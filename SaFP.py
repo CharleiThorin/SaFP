8d3d89b181bbd91063e78ddf60fddb23ed26fff6
Kiwabs Charles <ckiwabs@gmail.com>
2019-04-11 16:00:06 +0300

    permissions on user roles

--- SaFP.py
+++ SaFP.py
@@ -1,7 +1,7 @@
 import os
 from app import create_app, db
 from flask_migrate import Migrate
-from app.models import User, Role
+from app.models import User, Role, Permission
 
 
 app = create_app(os.getenv('SAFP_CONFIG') or 'default')
@@ -10,4 +10,4 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
+    return dict(db=db, User=User, Role=Role, Permission=Permission)
